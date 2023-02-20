471ca93e5297cdf489b1aa09dbac58a81995b489
Razerdp <razerdp123@gmail.com>
2023-02-20 16:34:34 +0800

    Add files via upload

--- github_actions.py
+++ github_actions.py
@@ -1,10 +1,78 @@
 # -*- coding: utf-8 -*-
 # created by fengweichao on 2023/2/20
-import rpg_filter
 import steam.game_servers as gs
 import datetime
 import json
 import os
+
+DEFAULT_KEYS = '弑神巅峰;RPG;杀戮世界;橡子杀戮;幻想杀戮;天域;午夜狂欢;猎人;帝王;风花雪月;暗黑之魂;无法逃脱;AK0048;梦幻天堂;腐尸之地;紫荆之巅;上帝之手;经典怀旧服;无人永生;星缘天空;破晓;混乱纪元;原生之初'
+DEFAULT_RATIO = 25
+
+
+def strKey2List(str_keys):
+    str_keys = str_keys.replace('；', ';')
+    str_keys = str_keys.replace('：', ':')
+    ret = []
+    for k in str_keys.split(';'):
+        ratio = DEFAULT_RATIO
+        if ':' in k:
+            name, t_r = k.split(':')
+            try:
+                ratio = int(t_r)
+            except:
+                ratio = DEFAULT_RATIO
+        else:
+            name = k
+        if name:
+            ret.append((name, int(max(0, min(ratio, 100)))))
+    return ret
+
+
+def serverInfo(checker_keys, server_addr):
+    if not server_addr:
+        return None
+    ret = None
+    server_info = None
+    try:
+        server_info = gs.a2s_info(server_addr)
+        if server_info:
+            name = server_info.get('name')
+            if name:
+                for check_name, check_ratio in checker_keys:
+                    score = fuzz.ratio(name, check_name)
+                    if score >= check_ratio:
+                        ret = {
+                            'ip': server_addr[0],
+                            'port': server_addr[1],
+                            'score': score,
+                            'name': name
+                        }
+                        break
+    except:
+        pass
+    return ret, server_info
+
+
+def scan(
+    checker_keys,
+    count,
+    region=gs.MSRegion.Asia,
+):
+    result = []
+    checker_keys = list(checker_keys)
+    cur_idx = 0
+    try:
+        for server_addr in gs.query_master(
+            r'\appid\550', max_servers=count, region=region, timeout=1
+        ):
+            cur_idx += 1
+            ret, server_info = serverInfo(checker_keys, server_addr)
+            ret_cb and ret_cb(ret)
+            if ret:
+                result.append(ret)
+    except:
+        pass
+    return result
 
 
 def createIpBlackList(scan_list):
@@ -28,9 +96,7 @@ def createIpBlackList(scan_list):
     return ret
 
 
-ret = rpg_filter.scan(
-    rpg_filter.strKey2List(rpg_filter.DEFAULT_KEYS), 10000, gs.MSRegion.World
-)
+ret = scan(strKey2List(DEFAULT_KEYS), 10000, gs.MSRegion.World)
 if ret:
     date = datetime.datetime.strftime(datetime.datetime.now(), '%Y_%m_%d')
     if not os.path.isdir('./IP_BLOCKER'):
