9b2af7d90697808d4018a849ef1b9b3259ffd947
Razerdp <razerdp123@gmail.com>
2023-02-20 16:59:01 +0800

    Add files via upload

--- github_actions.py
+++ github_actions.py
@@ -4,6 +4,7 @@ import steam.game_servers as gs
 import datetime
 import json
 import os
+import traceback
 
 DEFAULT_KEYS = '弑神巅峰;RPG;杀戮世界;橡子杀戮;幻想杀戮;天域;午夜狂欢;猎人;帝王;风花雪月;暗黑之魂;无法逃脱;AK0048;梦幻天堂;腐尸之地;紫荆之巅;上帝之手;经典怀旧服;无人永生;星缘天空;破晓;混乱纪元;原生之初'
 DEFAULT_RATIO = 25
@@ -49,7 +50,7 @@ def serverInfo(checker_keys, server_addr):
                         }
                         break
     except:
-        pass
+        traceback.print_exc()
     return ret, server_info
 
 
@@ -67,7 +68,6 @@ def scan(
         ):
             cur_idx += 1
             ret, server_info = serverInfo(checker_keys, server_addr)
-            ret_cb and ret_cb(ret)
             if ret:
                 result.append(ret)
     except:
