710342d7249091aa1f3d26cb9496a1cdd3bb9c62
razerdp <razerdp123@gmail.com>
2023-02-22 00:35:34 +0800

    亚洲

--- github_actions.py
+++ github_actions.py
@@ -131,7 +131,7 @@ class CalThread(threading.Thread):
 
 thread = CalThread()
 thread.start()
-ret = scan(strKey2List(DEFAULT_KEYS), 100000, gs.MSRegion.World)
+ret = scan(strKey2List(DEFAULT_KEYS), 30000, gs.MSRegion.Asia)
 if ret:
     date = datetime.datetime.strftime(datetime.datetime.now(), '%Y_%m_%d')
     if not os.path.isdir('./IP_BLOCKER'):
